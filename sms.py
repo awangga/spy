521f03ba93e29a029515571767bffa40e08bbde1
Rolly Maulana Awangga <rolly@awang.ga>
2015-09-30 14:26:02 +0700

    add read sms class

--- sms.py
+++ sms.py
@@ -6,10 +6,14 @@ import config
 import serial
 import time
 
-class Sms:
+class Send(object):
     def __init__(self, recipient=config.recipient, message=config.message):
+        self.ser = serial.Serial(config.serial, 115200, timeout=5)
+        self.ser.write('ATZ\r')
+        self.ser.write('AT+CMGF=1\r')
         self.recipient = recipient
         self.content = message
+        
 
     def setRecipient(self, number):
         self.recipient = number
@@ -18,13 +22,11 @@ class Sms:
         self.content = message
 
     def connectPhone(self):
-        self.ser = serial.Serial(config.serial, 460800, timeout=5)
+        
         time.sleep(1)
 
     def sendMessage(self):
-        self.ser.write('ATZ\r')
-        time.sleep(1)
-        self.ser.write('AT+CMGF=1\r')
+        
         time.sleep(1)
         self.ser.write('''AT+CMGS="''' + self.recipient + '''"\r''')
         time.sleep(1)
@@ -35,3 +37,41 @@ class Sms:
 
     def disconnectPhone(self):
         self.ser.close()
+
+class Read(object):
+
+    def __init__(self):
+        self.open()
+
+    def open(self):
+        self.ser = serial.Serial(config.serial, 115200, timeout=5)
+        self.SendCommand('ATZ\r')
+        self.SendCommand('AT+CMGF=1\r')
+
+    def SendCommand(self,command, getline=True):
+        self.ser.write(command)
+        data = ''
+        if getline:
+            data=self.ReadLine()
+        return data 
+
+    def ReadLine(self):
+        data = self.ser.readline()
+        print data
+        return data 
+
+    def GetAllSMS(self):
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        command = 'AT+CMGL="REC UNREAD"\r\n'#gets incoming sms that has not been read
+        print self.SendCommand(command,getline=True)
+        data = self.ser.readall()
+        print data
+        
+    def GetReadSMS(self):
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        command = 'AT+CMGL="REC READ"\r\n'#gets incoming sms that has not been read
+        print self.SendCommand(command,getline=True)
+        data = self.ser.readall()
+        print data
