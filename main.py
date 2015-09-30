b7f9196bdf826ed76b6c998ada7566e94da7ff12
Rolly Maulana Awangga <rolly@awang.ga>
2015-09-30 20:40:37 +0700

    add x

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 """
 main.py - Main Program to send txt messages.
 """
-import sms
+import smspdu
 psn = "Neng...!!! ieu mamah pake HP batur.. mamah keur aya masalah di kantor polisi.. mamah menta tulung pang nganteurkeun cai sa ember... inget cai sa ember ... lain pulsa...pulsa mah loba keneh...mamah keur milu ngising di kantor polisi... caina saat ... burukeun ulah seuri... mamah can cebok yeuh, inget nya neng.. cai sa ember ..!!! lain pulsa...!!! mamah geus cangkeul nagog"
 
-sms = sms.Sms("081320380477",psn)
+sms = smspdu.Smspdu("081320380477",psn)
 sms.send()
-sms = sms.Sms("081390320212",psn)
+sms = smspdu.Smspdu("081390320212",psn)
 sms.send()
-sms = sms.Sms("089610707901",psn)
+sms = smspdu.Smspdu("089610707901",psn)
+sms.send()
+sms = smspdu.Smspdu("08997194777",psn)
+sms.send()
+sms = smspdu.Smspdu("089697217506",psn)
+sms.send()
+sms = smspdu.Smspdu("085236041995",psn)
 sms.send()
 sms.unread()
 sms.disconnect()
