84cf2d32e2e70f72f5d65b6490665726f8fe7c2c
Rolly Maulana Awangga <rolly@awang.ga>
2015-10-01 07:01:15 +0700

    #fix add +62 for every destination

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
 """
-main.py - Main Program to send txt messages.
+main.py - Main Program to send txt messages. harus pake +62 menggantikan 0 di depan nomor
 """
 import smspdu
 import config
 
 
-sms = smspdu.Smspdu("081320380477",config.message)
+#sms = smspdu.Smspdu("+6281320380477",config.message)
+#sms.send()
+#sms = smspdu.Smspdu("+6281390320212",config.message)
+#sms.send()
+sms = smspdu.Smspdu("+6289610707901",config.message)
 sms.send()
-sms = smspdu.Smspdu("081390320212",config.message)
+sms = smspdu.Smspdu("+628997194777",config.message)
 sms.send()
-sms = smspdu.Smspdu("089610707901",config.message)
+sms = smspdu.Smspdu("+6289697217506",config.message)
 sms.send()
-sms = smspdu.Smspdu("08997194777",config.message)
+#sms = smspdu.Smspdu("+6285236041995",config.message)
+#sms.send()
+sms = smspdu.Smspdu("+6281320708675",config.message)
 sms.send()
-sms = smspdu.Smspdu("089697217506",config.message)
+sms = smspdu.Smspdu("+6285624874074",config.message)
 sms.send()
-sms = smspdu.Smspdu("085236041995",config.message)
+sms = smspdu.Smspdu("+6285703006269",config.message)
+sms.send()
+sms = smspdu.Smspdu("+6285221313763",config.message)
+sms.send()
+sms = smspdu.Smspdu("+6289655231804",config.message)
+sms.send()
+sms = smspdu.Smspdu("+6282217173862",config.message)
+sms.send()
+sms = smspdu.Smspdu("+6285763983023",config.message)
 sms.send()
 
