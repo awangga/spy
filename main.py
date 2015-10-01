1413fa29e9c2e1dd0bd780db00b11b8bc0f5099d
Rolly Maulana Awangga <rolly@awang.ga>
2015-10-01 11:29:23 +0700

    fix bug u kelebihan

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import smspdu
 import config
 
 
-sms = smspdu.Smspdu("+6281320380477",config.message)#yogo
-sms.send()
-sms.rcpt("+6281390320212")#
-sms.send()
-sms.rcpt("+6289610707901")
-sms.send()
-sms.rcpt("+628997194777")
-sms.send()
-sms.rcpt("+6289697217506")
-sms.send()
-sms.rcpt("+6285236041995")
-sms.send()
-sms.rcpt("+6281320708675")#sebelah kiri
-sms.send()
-sms.rcpt("+6285624874074")#beny prast
-sms.send()
-sms.rcpt("+6285703006269")#adam
-sms.send()
+#sms = smspdu.Smspdu("+6281320380477",config.message)#yogo
+#sms.send()
+#sms.rcpt("+6281390320212")#
+#sms.send()
+#sms.rcpt("+6289610707901")
+#sms.send()
+#sms.rcpt("+628997194777")
+#sms.send()
+#sms.rcpt("+6289697217506")
+#sms.send()
+#sms.rcpt("+6285236041995")
+#sms.send()
+#sms.rcpt("+6281320708675")#sebelah kiri
+#sms.send()
+#sms.rcpt("+6285624874074")#beny prast
+#sms.send()
+#sms.rcpt("+6285703006269")#adam
+#sms.send()
 sms.rcpt("+6285221313763")
 sms.send()
 sms.rcpt("+6289655231804")
@@ -32,7 +32,7 @@ sms.rcpt("+6282217173862")
 sms.send()
 sms.rcpt("+6285763983023")
 sms.send()
-sms.rcptu("+6289668923560")
+sms.rcpt("+6289668923560")
 sms.send()
 sms.rcpt("+6285775080595")
 sms.send()
