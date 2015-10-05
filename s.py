343f7cdbe02224a72302af9470ff49eaeb6dc498
Rolly Maulana Awangga <rolly@awang.ga>
2015-10-05 15:11:26 +0700

    add web service on cgi

---
+++ s.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+print "Content-Type: text-html"
+print
+import cgitb
+cgitb.enable()
+import cgi
+import smspdu
+form = cgi.FieldStorage()
+
+sms = smspdu.Smspdu(form["rcpt"].value,form["msg"].value)
+sms.send()
+sms.close()
+
