6b06495fa811ca537bcef2fe5e4cabf0c69a5eeb
Rolly Maulana Awangga <rolly@awang.ga>
2015-10-19 22:55:50 +0700

    cleaning code

--- libs.py
+++ libs.py
@@ -46,21 +46,10 @@ class Libs(object):
         for xpdu in self.pdu.to_pdu():
 	        command = 'AT+CMGS=%d\r' % xpdu.length
 	        self.SendCommand(command,len(str(xpdu.length))+14)
-	        #data = self.ser.readall()
-	        #data = self.ser.read(128)
-	        #print data
-	        #self.logfile.write(str(time.clock()))
-	        #self.logfile.write('after send readall 1 \n')
 	        command = '%s\x1a' % xpdu.pdu
 	        self.SendCommand(command,len(xpdu.pdu)+20)
-	        #print self.ser.readline()
-	        #data = self.ser.readall()
-	        #data = self.Read('\n')
-	        #print data
 	        self.logfile.write(str(datetime.now()))
 	        self.logfile.write('   after send a sms \n')
-		#data = areadline+breadline
-		#return data
 
     def close(self):
         self.ser.close()
@@ -76,77 +65,23 @@ class Libs(object):
         self.logfile.write('   after send command init \n')
         return data 
         
-    def SendCommands(self,command,n,getline=True):
-        self.logfile.write(str(datetime.now()))
-        self.logfile.write('   before send command init \n')
-        self.ser.write(command)
-        data = ''
-        if getline:
-            data=self.ReadLines(n)
-        self.logfile.write(str(datetime.now()))
-        self.logfile.write('   after send command init \n')
-        return data 
-        
     def ReadAll(self):
     	data = self.ser.readall()
     	return data
     
     def ReadLine(self,char):
-        #bytesToRead = self.ser.inWaiting()
-        #data = self.ser.read(bytesToRead)
         data = self.ser.read(char)
-        #data = self.ser.readline()
-        #data += self.ser.readline()
-        #data += self.ser.readline()
-        #data += self.ser.readline()
         if 'OK' in data:
         	print ' berhasil '
         if 'ERROR' in data:
         	print ' gagal '
         print data+' char:<'+str(char)+'> '
         return data
-        
-    def ReadLines(self,n):
-        #bytesToRead = self.ser.inWaiting()
-        #data = self.ser.read(bytesToRead)
-        #data = self.ser.read(char)
-        data = ''
-        #data += self.ser.readline()
-        #data += self.ser.readline()
-        #data += self.ser.readline()
-        i=0
-        while i < n:
-        	data += self.ser.readline()
-        	print ' readline-> '+str(i)+' <- '
-        	i += 1
-        #if 'OK' in data:
-        #	print ' berhasil '
-        #if 'ERROR' in data:
-        #	print ' gagal '
-        print data
-        return data
-        
-    def ReadStatus(self):
-        data = ''
-        while (not ('OK' in data)) or (not ('ERROR' in data)):
-        	data += self.ser.readline()
-        print data
-        return data
-        
-    def Read(self,term):
-        matcher = re.compile(term) # search anything
-        buff = ''
-        tic = time.clock()
-        buff += self.ser.read(128)
-        #while ((time.clock - tic) < config.timeout) and (not matcher.search(buff)):
-        while ((time.clock() - tic) < config.timeout) and (not matcher.search(buff)):
-        	buff += self.ser.read(128)
-        return buff
 
     def unreadMsg(self):
         self.ser.flushInput()
         self.ser.flushOutput()
-        command = 'AT+CMGL=0\r\n'#gets incoming sms that has not been read
+        command = 'AT+CMGL=0\r\n'
         self.SendCommand(command,getline=True)
         data = self.ser.readall()
         print data
@@ -154,7 +89,7 @@ class Libs(object):
     def readMsg(self):
         self.ser.flushInput()
         self.ser.flushOutput()
-        command = 'AT+CMGL=1\r\n'#gets incoming sms that has not been read
+        command = 'AT+CMGL=1\r\n'
         self.SendCommand(command,getline=True)
         data = self.ser.readall()
         print data
@@ -162,7 +97,7 @@ class Libs(object):
     def allMsg(self):
         self.ser.flushInput()
         self.ser.flushOutput()
-        command = 'AT+CMGL=4\r\n'#gets incoming sms that has not been read
+        command = 'AT+CMGL=4\r\n'
         self.SendCommand(command,getline=True)
         data = self.ser.readall()
         print data
